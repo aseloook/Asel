6702914ce9159b71752054c58d529ec6da1ee630
aseloook <drokhmanov@internet.ru>
2024-02-10 15:20:30 +0600

    hellou

--- src/main/java/pr/User.java
+++ src/main/java/pr/User.java
@@ -3,7 +3,7 @@ package pr;
 public class User {
     public boolean isValidUserCredentials(String sUserName,String sUserPAssword)
     {
-        if(sUserName.equals("assel")&& sUserPAssword.equals("123"))
+        if(sUserName.equals("asselok")&& sUserPAssword.equals("123"))
         {
             return true;
         }
